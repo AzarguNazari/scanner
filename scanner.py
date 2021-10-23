7f5291637726141e2fae6e2bb00e8d358b7ce785
Hazar Gul Nazari <nazariazargul@gmail.com>
2021-10-23 16:56:54 +0200

    Create scanner.py

---
+++ scanner.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import os,sys,time,pexpect
+
+def findaddress():
+  address=''
+  p = pexpect.spawn('hcitool scan', encoding='utf-8')
+  p.logfile_read = sys.stdout
+  mylist = ['E4\:17\:D8\:[0-9A-F].[:][0-9A-F].[:][0-9A-F].', '16\:04\:18\:[0-9A-F].[:][0-9A-F].[:][0-9A-F].',pexpect.EOF]
+  p.expect(mylist)
+  address=p.after
+  if address==pexpect.EOF:
+    return ''
+  else:
+    return address
+
+def setbt(address):
+  response=''
+  p = pexpect.spawn('bluetoothctl', encoding='utf-8')
+  p.logfile_read = sys.stdout
+  p.expect('#')
+  p.sendline("remove "+address)
+  p.expect("#")
+  p.sendline("scan on")
+
+  mylist = ["Discovery started","Failed to start discovery","Device "+address+" not available","Failed to connect","Connection successful"]
+  while response != "Connection successful":
+    p.expect(mylist)
+    response=p.after
+    p.sendline("connect "+address)
+    time.sleep(1)
+  p.sendline("quit")
+  p.close()
+  #time.sleep(1)
+  return
+
+
+address='' 
+while address=='':
+  address=findaddress()
+  time.sleep(1)
+  
+print (address," found")
+setbt(address)
